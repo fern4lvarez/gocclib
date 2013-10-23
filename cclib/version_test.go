6227f79a249f2ec0b7ee6125f279dc3083ff26a1
Fernando Álvarez <alvarez.calleja.fernando@gmail.com>
2013-10-23 15:27:08 +0200

    Version 0.0.4 - Manage user keys - Better documentation

--- cclib/version_test.go
+++ cclib/version_test.go
@@ -9,7 +9,7 @@ func TestVersion(t *testing.T) {
 	v := Version()
 
 	// Then
-	if v != "0.0.3" {
-		t.Errorf(msgFail, "Version", "0.0.3", v)
+	if v != "0.0.4" {
+		t.Errorf(msgFail, "Version", "0.0.4", v)
 	}
 }
