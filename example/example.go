2d02ba029ce224d030222e83a9a0d9e52d961001
Fernando Álvarez <alvarez.calleja.fernando@gmail.com>
2013-11-04 16:06:58 +0100

    Version 0.1.0 - Handle Logs - Handle billing accounts - Skip SSL Certification - Set root CAs - Add DEBUG mode - Add Travis CI - Extend documentation

--- example/example.go
+++ example/example.go
@@ -9,18 +9,9 @@ import (
 
 func main() {
 	api := cc.NewAPI()
-	err := api.CreateTokenFromFile("/home/fa/.cc")
+	err := api.CreateTokenFromFile("filepath")
 	if err != nil {
 		fmt.Printf("%s\n", err.Error())
 		os.Exit(0)
 	}
-
-	/*	fago, _ := api.ReadDeployment("fago", "default")
-		fmt.Println(fago)*/
-
-	// faKey, _ := api.ReadUserKeys("fernandoalvarez")
-	//fmt.Println(faKey)
-
-	ba, _ := api.ReadBillingAccounts("fernandoalvarez")
-	fmt.Println(ba)
 }
