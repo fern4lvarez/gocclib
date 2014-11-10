79d8b5957d24b333e80ae25a388f6e0eaccad63f
Fernando Álvarez <fa@cloudcontrol.de>
2014-11-10 17:04:25 +0100

    Update documentation

--- cclib/api.go
+++ cclib/api.go
@@ -7,18 +7,55 @@ Basic usage example:
 	package main
 
 	import (
+		"fmt"
 		"os"
 
 		cc "github.com/fern4lvarez/gocclib/cclib"
 	)
 
 	func main() {
+		// Create API instance
 		api := cc.NewAPI()
-		err := api.CreateToken("user@email.org", "password")
+
+		// Create new user
+		john, err := api.CreateUser("john", "john@example.org", "secret")
 		if err != nil {
-			os.Exit(0)
+			fmt.Println(err)
+			os.Exit(1)
 		}
-		...
+		fmt.Println(john.Username, "was created.")
+
+		// Activate User with the code provided by email
+		john, err = api.ActivateUser("json", "activationcode")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println(john.Username, "is active.")
+
+		// Basic authentication to API
+		err = api.CreateTokenFromFile("filepath")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println("Authenticated as", john.Username)
+
+		// Create Application
+		myapp, err := api.CreateApplication("myapp", "ruby", "git", "")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println(myapp.Name, "was created.")
+
+		// Add user to the application
+		_, err = api.CreateAppUser("myapp", "john@example.org", "")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println(john.Username, "was added to", myapp.Name)
 	}
 
 */
--- example/example.go
+++ example/example.go
@@ -8,10 +8,46 @@ import (
 )
 
 func main() {
+	// Create API instance
 	api := cc.NewAPI()
-	err := api.CreateTokenFromFile("filepath")
+
+	// Create new user
+	john, err := api.CreateUser("john", "john@example.org", "secret")
 	if err != nil {
-		fmt.Printf("%s\n", err.Error())
-		os.Exit(0)
+		fmt.Println(err)
+		os.Exit(1)
 	}
+	fmt.Println(john.Username, "was created.")
+
+	// Activate User with the code provided by email
+	john, err = api.ActivateUser("json", "activationcode")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(john.Username, "is active.")
+
+	// Basic authentication to API
+	err = api.CreateTokenFromFile("filepath")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("Authenticated as", john.Username)
+
+	// Create Application
+	myapp, err := api.CreateApplication("myapp", "ruby", "git", "")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(myapp.Name, "was created.")
+
+	// Add user to the application
+	_, err = api.CreateAppUser("myapp", "john@example.org", "")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(john.Username, "was added to", myapp.Name)
 }
